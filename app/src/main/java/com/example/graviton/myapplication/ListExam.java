7c15a308fc68b95a311f63d074358ab5f3a7fb79
graviton <tafirmoon@gmail.com>
2016-06-08 11:03:59 +0600

    near final

---
+++ app/src/main/java/com/example/graviton/myapplication/ListExam.java
@@ -0,0 +1,13 @@
+package com.example.graviton.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListExam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_exam);
+    }
+}
